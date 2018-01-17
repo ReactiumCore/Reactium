5265bc615279b80e96cb4d3a1d11be2026c7a58b
Cameron Tullos <ctullos@resourceammirati.com>
2018-01-17 17:49:00 -0500

    Removed design system files.

--- src/assets/design-system/src/assets/ar/scripts/ar.js
+++
@@ -1,9 +0,0 @@
-
-require('./ar-btn-group');
-require('./prism');
-require('./colors');
-require('./toggler');
-require('./search');
-require('./nanoscroll');
-require('./menu');
-require('./code');
--- src/assets/design-system/src/assets/ar/scripts/code.js
+++
@@ -1,4 +0,0 @@
-const Clipboard = require('clipboard');
-
-const btnCB = new Clipboard('[data-clipboard-target]');
-
