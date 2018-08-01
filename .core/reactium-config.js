8da25b68a7c88eb17d96a23f8697f64099af6e9e
Cam Tullos <cam@tullos.ninja>
2018-08-01 10:09:10 -0400

    Added update object to reactium-config so that the package.json file can be updated per need.

--- .core/reactium-config.js
+++ .core/reactium-config.js
@@ -7,5 +7,23 @@
  */
 module.exports = {
     version: '2.3.4',
-    semver: '2.3.x'
+    semver: '2.3.x',
+    update: {
+        package: {
+            devDependencies: {
+                add: {
+                    'gulp-run': '^1.7.1'
+                },
+                remove: {}
+            },
+            dependencies: {
+                add: {},
+                remove: {}
+            },
+            scripts: {
+                add: {},
+                remove: {}
+            }
+        }
+    }
 };
