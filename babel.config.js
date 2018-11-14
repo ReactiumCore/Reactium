01026603a4b6ef9d1d5acd9fd75fd305799bdff4
John Dillick <john@dillick.us>
2018-11-13 21:18:53 -0500

    babel.config.js with example for babel resolver plugin.

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,12 @@
 const config = require('./.core/babel.config');
 
+// @example
+//
+// To add a module resolver for node and webpack
+//
+// const path = require('path');
+//
+// const moduleResolver = config.plugins.find(plugin => plugin[0] === 'module-resolver');
+// moduleResolver[1].alias['redux-addons'] = './src/app/redux-addons';
+
 module.exports = config;
