4af732f3a7dd92e27104b2bb01f8a9a9115263f1
John A Dillick <jdillick@updox.com>
2019-01-31 16:05:14 -0500

    Make default scroll to top behavior in RouteObserver extensible/overrideable.

--- .core/components/Router/actions.js
+++ .core/components/Router/actions.js
@@ -5,27 +5,39 @@ export default {
     updateRoute: (location, route = {}, params) => (dispatch, getState) => {
         const { Router } = getState();
 
+        const defaultOnRouteChange = () => {
+            if (
+                typeof window !== 'undefined' &&
+                Router.pathname !== location.pathname
+            ) {
+                window.scrollTo(0, 0);
+            }
+        };
+
+        // call defaultOnRouteChange, or route's onRouteChage (with defaultOnRouteChange as arg)
+        const { onRouteChange = defaultOnRouteChange } = route;
         if (
+            typeof onRouteChange === 'function' &&
             typeof window !== 'undefined' &&
             Router.pathname !== location.pathname
         ) {
-            window.scrollTo(0, 0);
+            onRouteChange(defaultOnRouteChange);
         }
-
-        dispatch({
-            type: deps.actionTypes.UPDATE_ROUTE,
-            location,
-            params
-        });
 
         // load route data
         if ('load' in route) {
             dispatch(
                 route.load(
                     params,
-                    queryString.parse(location.search.replace(/^\?/, ''))
-                )
+                    queryString.parse(location.search.replace(/^\?/, '')),
+                ),
             );
         }
-    }
+
+        dispatch({
+            type: deps.actionTypes.UPDATE_ROUTE,
+            location,
+            params,
+        });
+    },
 };
