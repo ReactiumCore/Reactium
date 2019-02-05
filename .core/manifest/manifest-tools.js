43432866da01773a9a991a2738893cf58baea54d
John Dillick <john@dillick.us>
2019-02-05 12:07:13 -0500

    Update manifest-tools.js

--- .core/manifest/manifest-tools.js
+++ .core/manifest/manifest-tools.js
@@ -82,7 +82,7 @@ module.exports = function({
             }),
         ),
         {
-            parser: 'babel',
+            parser: 'babylon',
             trailingComma: 'all',
             singleQuote: true,
             tabWidth: 4,
