7aa2b138803a5d27d8599376414121031accca10
Cam Tullos <cam@tullos.ninja>
2019-02-11 16:35:54 -0500

    Toolkit: applied fade in fx so that it doesn't feel so abrupt when you load a page.

--- .core/components/Toolkit/Loading/index.js
+++ .core/components/Toolkit/Loading/index.js
@@ -1,18 +1,36 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { TweenMax } from 'gsap/umd/TweenMax';
 
 /**
  * -----------------------------------------------------------------------------
  * Functional Component: Loading
  * -----------------------------------------------------------------------------
  */
-const Loading = ({ onComplete }) => {
-    setTimeout(onComplete, 725);
 
-    return (
-        <div className='re-toolkit-loading'>
-            <div className='loader' />
-        </div>
-    );
-};
+export default class Loading extends Component {
+    constructor(props) {
+        super(props);
 
-export default Loading;
+        this.container = React.createRef();
+        this.fadeOut = this.fadeOut.bind(this);
+    }
+
+    componentDidMount() {
+        this.fadeOut();
+    }
+
+    fadeOut() {
+        const { onComplete } = this.props;
+        const elm = this.container.current;
+
+        TweenMax.to(elm, 0.25, { opacity: 0, delay: 0.5, onComplete });
+    }
+
+    render() {
+        return (
+            <div className='re-toolkit-loading' ref={this.container}>
+                <div className='loader' />
+            </div>
+        );
+    }
+}
