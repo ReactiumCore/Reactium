7db55394d5cae424917f84ad26233d95028cc5b6
John A Dillick <john@dillick.us>
2019-04-04 16:33:27 -0400

    Add useStore and useSelect custom React hooks to reactium-core/easy-connect.

--- .core/easy-connect.js
+++ .core/easy-connect.js
@@ -1,10 +1,19 @@
+import { useContext } from 'react';
+import { connect, ReactReduxContext } from 'react-redux';
+import op from 'object-path';
+
 /**
  * An "easy connect" helper for redux.
  * Provides getState and dispatch from store using connect HoC
  */
 export const ec = Component => {
-    const { connect } = require('react-redux');
     return connect(state => ({
         getState: () => state,
     }))(Component);
 };
+
+/**
+ * Custom React hooks for accessing store or specific data from the store.
+ */
+export const useStore = () => op.get(useContext(ReactReduxContext), 'store');
+export const useSelect = select => op.get(useStore().getState(), select);
