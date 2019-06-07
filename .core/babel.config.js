b7def4b22fbf59ab5ca3a3d3fa31c74e5127af3d
John A Dillick <john@dillick.us>
2019-06-07 12:23:44 -0400

    Safely fix annoying babel core warning about useBuiltins with core-js.

--- .core/babel.config.js
+++ .core/babel.config.js
@@ -1,5 +1,32 @@
 const path = require('path');
 const rootPath = path.resolve(__dirname, '..');
+const semver = require('semver');
+const op = require('object-path');
+const babelCoreVersion = op.get(
+    require(path.resolve(
+        path.dirname(require.resolve('@babel/core')),
+        '../package.json',
+    )),
+    'version',
+);
+const coreJsVersion = op.get(
+    require(path.resolve(
+        path.dirname(require.resolve('core-js')),
+        'package.json',
+    )),
+    'version',
+);
+
+let corejs;
+// annoying warning starts with @babel/core 7.4.0
+if (semver.satisfies(semver.coerce(babelCoreVersion), '^7.4.0')) {
+    if (semver.satisfies(semver.coerce(coreJsVersion), '>=2.0.0')) {
+        corejs = '2';
+    }
+    if (semver.satisfies(semver.coerce(coreJsVersion), '>=3.0.0')) {
+        corejs = '3';
+    }
+}
 
 const alias = {
     manifest: './src/manifest',
@@ -10,20 +37,17 @@ const alias = {
     toolkit: './src/app/toolkit',
 };
 
+const env = {
+    ...(corejs ? { corejs } : {}),
+    useBuiltIns: 'usage',
+    debug: false,
+    targets: {
+        browsers: ['> 1%', 'IE 11'],
+    },
+};
+
 module.exports = {
-    presets: [
-        '@babel/react',
-        [
-            '@babel/env',
-            {
-                useBuiltIns: 'usage',
-                debug: false,
-                targets: {
-                    browsers: ['> 1%', 'IE 11'],
-                },
-            },
-        ],
-    ],
+    presets: ['@babel/react', ['@babel/env', env]],
     plugins: [
         ['@babel/plugin-syntax-dynamic-import'],
         [
