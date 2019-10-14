e110937a434e7085dd0be8bfdd9250ffad559d10
John Dillick <john@dillick.us>
2019-10-14 16:40:44 -0400

    Add .cli ignore to default actions manifest.

--- .core/reactium-config.js
+++ .core/reactium-config.js
@@ -81,6 +81,7 @@ const defaultManifestConfig = {
             name: 'allActions',
             type: 'actions',
             pattern: /actions.jsx?$/,
+            ignore: /\.cli/,
         },
         {
             name: 'allActionTypes',
