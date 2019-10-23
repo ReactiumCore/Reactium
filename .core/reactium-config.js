de88504dc835475f76bd1af70fb449b44df77e32
Cam Tullos <cam@tullos.ninja>
2019-10-23 16:07:06 -0400

    version bump

--- .core/reactium-config.js
+++ .core/reactium-config.js
@@ -209,7 +209,7 @@ const manifestConfig = manifestConfigOverride(defaultManifestConfig);
  * @type {Object}
  */
 module.exports = {
-    version: '3.1.0',
+    version: '3.1.1',
     semver: '^3.0.0',
     build: gulpConfig,
     update: {
