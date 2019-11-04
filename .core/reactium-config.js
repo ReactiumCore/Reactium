3baaf57926d4804f4d2c34715f7bbde6cbc9a742
Cam Tullos <cam@tullos.ninja>
2019-11-04 15:53:26 -0500

    version bump for Reactium SDK changes.

--- .core/reactium-config.js
+++ .core/reactium-config.js
@@ -209,7 +209,7 @@ const manifestConfig = manifestConfigOverride(defaultManifestConfig);
  * @type {Object}
  */
 module.exports = {
-    version: '3.1.5',
+    version: '3.1.6',
     semver: '^3.0.0',
     build: gulpConfig,
     update: {
