2b0cbf53fc2778cb0ab69cef103e1e72a097c3f0
John Dillick <john@dillick.us>
2019-11-06 16:54:21 -0500

    useHookComponent() apidocs

--- .core/sdk/component/index.js
+++ .core/sdk/component/index.js
@@ -3,23 +3,50 @@ import Hook from '../hook';
 const Component = {};
 
 /**
- * @api {Function} Component.register(hook,component,order) Register a component to be used with useHookComponent.
+ * @api {Function} Component.register(hook,component,order) Component.register
  * @apiGroup Reactium.Component
  * @apiName Component.register
  * @apiDescription Register a React component to be used with a specific useHookComponent hook. This must be called before the useHookComponent that defines the hook.
  * @apiParam {String} hook The hook name
  * @apiParam {Mixed} component component(s) to be output by useHookComponent
  * @apiParam {Number} order precedent of this if Component.register is called multiple times (e.g. if you are trying to override core or another plugin)
- * @apiExample Example Usage:
+ * @apiExample reactium-hooks.js
 import React from 'react';
 import Reactium from 'reactium-core/sdk';
 
-const MyComponent = props => (
-    <div>
-        {props.value}
-    </div>
-);
-Reactium.Component.register('my-component', MyComponent);
+// component to be used unless overriden by Reactium.Component.register()
+const ReplacementComponentA = () => <div>My Plugin's Component</div>
+const ReplacementComponentB = () => <div>My Alternative Component</div>
+
+// Simple Version
+Reactium.Component.register('my-component', ReplacementComponentA);
+
+// Advanced Form using Reactium.Hook SDK
+Reactium.Hook.register('my-component', async (...params) => {
+    const context = params.pop(); // context is last argument
+    const [param] = params;
+    if (param === 'test') {
+        context.component = ReplacementComponentA;
+    } else {
+        context.component = ReplacementComponentB;
+    }
+}
+})
+ * @apiExample parent.js
+import React from 'react';
+import { useHookComponent } from 'reactium-core/sdk';
+
+// component to be used unless overriden by Reactium.Component.register()
+const DefaultComponent = () => <div>Default or Placeholder component</div>
+
+export props => {
+    const MyComponent = useHookComponent('my-component', DefaultComponent, 'test');
+    return (
+        <div>
+            <MyComponent {...props} />
+        </div>
+    );
+};
  */
 Component.register = (hook, component, order) =>
     Hook.register(
@@ -36,7 +63,7 @@ Component.register = (hook, component, order) =>
  * @api {Function} Component.unregister(uuid) Alias for Reactium.Hook.unregister.
  * @apiGroup Reactium.Component
  * @apiName Component.unregister
- * @apiDescription Unregister a component used in useHookComponent. This must be performed before useHookComponent is called.
+ * @apiDescription Unregister a component used in useHookComponent. This must be performed before useHookComponent is called. See Hook.unregister()
  * @apiParam {String} uuid the registered component hook uuid
  */
 Component.unregister = Hook.unregister;
--- .core/sdk/react/component.js
+++ .core/sdk/react/component.js
@@ -2,10 +2,42 @@ import Hook from '../hook';
 import { useState, useEffect, useRef } from 'react';
 import op from 'object-path';
 
+/**
+ * @api {Hook} useHookComponent(hookName,defaultComponent,...params) useHookComponent()
+ * @apiDescription A React hook used to define React component(s) that can be overrided by Reactium plugins, using the Reactium.Component.register() function.
+ * @apiParam {String} hookName the unique string used to register component(s).
+ * @apiParam {Component} defaultComponent the default React component(s) to be returned by the hook.
+ * @apiParam {Mixed} params variadic list of parameters to be passed to the Reactium hook specified by hookName.
+ * @apiName useHookComponent
+ * @apiGroup ReactHook
+ * @apiExample parent.js
+import React from 'react';
+import { useHookComponent } from 'reactium-core/sdk';
+
+// component to be used unless overriden by Reactium.Component.register()
+const DefaultComponent = () => <div>Default or Placeholder component</div>
+
+export props => {
+    const MyComponent = useHookComponent('my-component', DefaultComponent);
+    return (
+        <div>
+            <MyComponent {...props} />
+        </div>
+    );
+};
+* @apiExample reactium-hooks.js
+import React from 'react';
+import Reactium from 'reactium-core/sdk';
+
+// component to be used unless overriden by Reactium.Component.register()
+const ReplacementComponent = () => <div>My Plugin's Component</div>
+
+Reactium.Component.register('my-component', ReplacementComponent);
+ */
 export const useHookComponent = (
     hook = 'component',
     defaultComponent = () => null,
-    ...props
+    ...params
 ) => {
     const component = useRef(defaultComponent);
     const [, update] = useState(component.current);
@@ -18,7 +50,7 @@ export const useHookComponent = (
 
     useEffect(() => {
         const getComponent = async () => {
-            const context = await Hook.run(hook, ...props);
+            const context = await Hook.run(hook, ...params);
             setComponent(op.get(context, 'component'));
         };
         getComponent();
