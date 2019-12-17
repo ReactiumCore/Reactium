6f3eaf13e4273bbed432748a15ca40f9a05a5b1a
John Dillick <john@dillick.us>
2019-12-17 11:04:26 -0500

    Add notice if discontinued Redux "Plugin" actions are dispatched.
    Console log will suggest using the Reactium SDK instead.

---
+++ .core/components/Plugable/index.js
@@ -0,0 +1,6 @@
+export { Zone as Plugins } from '@atomic-reactor/reactium-sdk-core';
+export { Zone } from '@atomic-reactor/reactium-sdk-core';
+
+console.log(
+    "You imported from reactium-core/components/Plugable which is deprecated. Use useHookComponent('Zone') instead, or import { Zone } from reactium-core/components/Plugable/Zone",
+);
---
+++ .core/components/Plugable/reducers.js
@@ -0,0 +1,33 @@
+export default (state = {}, action) => {
+    switch (action.type) {
+        case 'ADD_PLUGIN':
+        case 'UPDATE_PLUGIN':
+        case 'REMOVE_PLUGIN':
+        case 'ADD_PLUGIN_CONTROLS':
+        case 'REMOVE_PLUGIN_CONTROLS':
+            console.log(
+                `action ${action.type} is no longer used. See one of the following Reactium.Zone SDK functions.`,
+            );
+            if (typeof window !== 'undefined') {
+                console.log(`See ${window.location.origin}/docs`);
+            }
+            console.log(
+                `Use Reactium.Zone.addComponent() instead of dispatching the ADD_PLUGIN action type.`,
+            );
+            console.log(
+                `Use Reactium.Zone.removeComponent() instead of dispatching the REMOVE_PLUGIN action type.`,
+            );
+            console.log(
+                `Use Reactium.Zone.addFilter() to add a filter function to a zone where your component is rendered.`,
+            );
+            console.log(
+                `Use Reactium.Zone.addMap() to add a mapping function to a zone where your component is rendered.`,
+            );
+            console.log(
+                `Use Reactium.Zone.addSort() to add sortBy criteria to a zone where your component is rendered.`,
+            );
+        default:
+            return state;
+    }
+    return state;
+};
