bf5f846f82feae16b78613ffbdfa80884dd6ce2e
Cam Tullos <cam@tullos.ninja>
2019-12-20 13:36:20 -0500

    Fixed Reactium SDK User.getSessionToken() function.

--- .core/reactium-config.js
+++ .core/reactium-config.js
@@ -3,7 +3,7 @@ const path = require('path');
 const rootPath = path.resolve(__dirname, '..');
 const gulpConfig = require('./gulp.config');
 
-const version = '3.1.19';
+const version = '3.1.20';
 
 const contextMode = () => {
     if (
