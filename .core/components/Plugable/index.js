0287b9f15abe5a8d6ef0f6624d9b0349b51f411d
John Dillick <john@dillick.us>
2020-01-03 12:56:38 -0500

    Update Plugins name change deprecation notice

--- .core/components/Plugable/index.js
+++ .core/components/Plugable/index.js
@@ -2,5 +2,5 @@ export { Zone as Plugins } from '@atomic-reactor/reactium-sdk-core';
 export { Zone } from '@atomic-reactor/reactium-sdk-core';
 
 console.log(
-    "You imported from reactium-core/components/Plugable which is deprecated. Use useHookComponent('Zone') instead, or import { Zone } from reactium-core/components/Plugable/Zone",
+    "You imported from reactium-core/components/Plugable which is deprecated. Use useHookComponent('Zone') instead, or import { Zone } from reactium-core/sdk",
 );
