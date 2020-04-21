5035e56a109839d20b5985358631089dec40146c
Cam Tullos <cam@tullos.ninja>
2020-04-20 20:31:24 -0400

    Updated prettierignore to exclude docs.

--- docs/api_project.js
+++ docs/api_project.js
@@ -8,7 +8,7 @@ define({
   "apidoc": "0.3.0",
   "generator": {
     "name": "apidoc",
-    "time": "2020-04-21T00:13:53.510Z",
+    "time": "2020-04-21T00:31:29.532Z",
     "url": "http://apidocjs.com",
     "version": "0.20.1"
   }
