be2fd36b33151e90ca8df771a769c278283e6380
John Dillick <john@dillick.us>
2020-07-10 18:42:16 -0400

    Flesh out and document Server renderer hooks and registries.

--- docs/api_project.js
+++ docs/api_project.js
@@ -8,7 +8,7 @@ define({
   "apidoc": "0.3.0",
   "generator": {
     "name": "apidoc",
-    "time": "2020-05-13T19:18:01.556Z",
+    "time": "2020-07-10T22:40:22.811Z",
     "url": "http://apidocjs.com",
     "version": "0.20.1"
   }
