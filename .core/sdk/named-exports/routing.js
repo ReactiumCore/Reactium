c34c62ac22caf94f551ddfc5d7ed7e9b6a1dbf0f
John Dillick <john@dillick.us>
2020-10-21 19:36:20 -0400

    Initialize useRouting() hook properly based on current state of Reactium.Routing.

--- .core/sdk/named-exports/routing.js
+++ .core/sdk/named-exports/routing.js
@@ -9,22 +9,51 @@ export const useRouting = () => {
         current: Routing.currentRoute,
         previous: Routing.previousRoute,
         active: Routing.currentRoute,
-        transitionState: 'READY',
-        transitionStates: [],
-        changes: {},
+        transitionState: Routing.transitionState || 'READY',
+        transitionStates: Routing.transitionStates || [],
+        changes: Routing.changes || {},
     });
 
-    const handler = updates => {
+    const handler = (updates, forceRefresh = true) => {
         routing.current = {
             ...routing.current,
             ...updates,
         };
-        update(new Date());
+
+        if (forceRefresh === true) update(new Date());
+    };
+
+    const refreshFromRouting = () => {
+        const {
+            active: activeLabel = 'current',
+            currentRoute,
+            previousRoute,
+            transitionState = 'READY',
+            transitionStates = [],
+            changes,
+        } = Routing;
+
+        const active =
+            activeLabel === 'previous' ? previousRoute : currentRoute;
+
+        handler(
+            {
+                current: currentRoute,
+                previous: previousRoute,
+                active,
+                transitionState,
+                transitionStates,
+                changes,
+            },
+            false,
+        );
     };
 
     useEffect(() => {
         const id = uuid();
         Routing.routeListeners.register(id, { handler });
+        refreshFromRouting();
+
         return () => {
             Routing.routeListeners.unregister(id);
         };
