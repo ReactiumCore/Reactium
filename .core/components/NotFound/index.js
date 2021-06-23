177a0dddd9b45748f037d5c07486d334767b9af4
Cam Tullos <>
2021-06-22 16:00:56 -1000

    Updated default NotFound component to be a Zone component with the id of 'not-found'.

--- .core/components/NotFound/index.js
+++ .core/components/NotFound/index.js
@@ -1,23 +1,24 @@
+import React, { useEffect } from 'react';
+import Reactium, { __, Zone } from 'reactium-core/sdk';
 
-/**
- * -----------------------------------------------------------------------------
- * Imports
- * -----------------------------------------------------------------------------
- */
-import React from 'react';
+const DefaultComponent = () => {
+    const comps = Reactium.Zone.getZoneComponents('not-found');
 
+    if (comps.length < 1) {
+        Reactium.Zone.addComponent({
+            zone: 'not-found',
+            id: 'NOT_FOUND_DEFAULT',
+            component: () => __('PAGE NOT FOUND'),
+            order: Reactium.Enums.priority.highest,
+        });
 
-/**
- * -----------------------------------------------------------------------------
- * Functional Component: NotFound
- * -----------------------------------------------------------------------------
- */
-const NotFound = (props) => {
-    return (
-        <div>
-            PAGE NOT FOUND
-        </div>
-    );
+        return () => {
+            Reactium.Zone.removeComponent('NOT_FOUND_DEFAULT');
+        };
+    }
 };
 
-export default NotFound;
+export default () => {
+    useEffect(DefaultComponent, []);
+    return <Zone zone='not-found' />;
+};
