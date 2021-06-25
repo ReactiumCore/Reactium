d83413c352f4e286e25e660c7509623b7db54d42
John Dillick <john@dillick.us>
2021-06-25 16:01:57 -0400

    Remove last redux dependency from core. @atomic-reactor/reactium-redux v1.0.2, including Router state and SDKs.

--- docs/api_project.js
+++ docs/api_project.js
@@ -8,7 +8,7 @@ define({
   "apidoc": "0.3.0",
   "generator": {
     "name": "apidoc",
-    "time": "2021-06-25T05:26:49.534Z",
+    "time": "2021-06-25T20:00:14.341Z",
     "url": "https://apidocjs.com",
     "version": "0.25.0"
   }
