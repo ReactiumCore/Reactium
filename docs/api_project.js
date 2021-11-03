f00f3ad3089580d11ae9373f9b59acc5dd0ea131
John Dillick <john@dillick.us>
2021-11-03 12:41:02 -0400

    @atomic-reactor/reactium-sdk-core@^1.2.7

--- docs/api_project.js
+++ docs/api_project.js
@@ -8,7 +8,7 @@ define({
   "apidoc": "0.3.0",
   "generator": {
     "name": "apidoc",
-    "time": "2021-10-29T19:35:27.113Z",
+    "time": "2021-11-03T16:40:16.165Z",
     "url": "https://apidocjs.com",
     "version": "0.25.0"
   }
