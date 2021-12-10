bb40c63d9ccce241f9cd2ee2837dc3a32608dd52
John Dillick <john@dillick.us>
2021-12-10 13:30:26 -0500

    Add persistHandle route prop to allow for handle data that persists in memory after loading a page an navigating away (front-end routing only)

--- .core/sdk/routing/index.js
+++ .core/sdk/routing/index.js
@@ -104,6 +104,11 @@ class Routing {
                         op.get(updates, 'previous.match.route.id', false)
                     );
                 })
+                .filter(
+                    ([id]) =>
+                        Handle.get(id) &&
+                        op.get(Handle.get(id), 'persistHandle', false) !== true,
+                )
                 .forEach(([id]) => {
                     Handle.unregister(id);
                 });
@@ -122,10 +127,18 @@ class Routing {
 
             if (typeof loadState === 'function') {
                 try {
-                    Handle.register(handleId, {
-                        routeId: op.get(updates, 'active.match.route.id'),
-                        current: new ReactiumSyncState({}),
-                    });
+                    const persistHandle = op.get(
+                        updates,
+                        'active.match.route.persistHandle',
+                        false,
+                    );
+                    if (!persistHandle || !Handle.get(handleId)) {
+                        Handle.register(handleId, {
+                            routeId: op.get(updates, 'active.match.route.id'),
+                            persistHandle,
+                            current: new ReactiumSyncState({}),
+                        });
+                    }
 
                     const route = op.get(updates, 'active.match.route', {});
 
