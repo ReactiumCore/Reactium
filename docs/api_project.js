1e49a20297192c857d88f108c0377a065cf7881b
John Dillick <john@dillick.us>
2022-02-17 11:37:04 -0500

    Patch and safe minor package updates (#125)

--- docs/api_project.js
+++ docs/api_project.js
@@ -8,7 +8,7 @@ define({
   "apidoc": "0.3.0",
   "generator": {
     "name": "apidoc",
-    "time": "2022-01-31T19:33:20.274Z",
+    "time": "2022-02-17T16:33:40.461Z",
     "url": "https://apidocjs.com",
     "version": "0.25.0"
   }
