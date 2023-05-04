b9b4df6b2bbe1778cc68d9fdd8b37b5a5740b7f9
John Dillick <john@dillick.us>
2023-05-04 13:24:16 -1000

    Core as module

--- .core/easy-connect.js
+++
@@ -1,6 +0,0 @@
-export * from 'reactium-core/sdk';
-
-console.log(
-    '%cImporting from `reactium-core/easy-connect` is deprecated. Import from `reactium-core/sdk` instead.',
-    'font-size: 12px; color: #F4F19C; background-color: #4F82BA',
-);
--- .core/sdk/named-exports/hookable-component.js
+++
@@ -1,6 +0,0 @@
-import { useHookComponent } from '@atomic-reactor/reactium-sdk-core';
-
-export const hookableComponent = name => props => {
-    const Component = useHookComponent(name);
-    return <Component {...props} />;
-};
