d00bfb0511657f2755b4f540331d2a486598c1c5
John Dillick <john@dillick.us>
2023-05-12 20:12:55 -0400

    HMR in src/app/main.js is no-longer needed.

--- src/app/main.js
+++ src/app/main.js
@@ -10,20 +10,4 @@ import { Shell } from '@atomic-reactor/reactium-core/app/shell';
         const { AppError } = await import('@atomic-reactor/reactium-core/app');
         await AppError(error);
     }
-
-    /**
-     * @description Initialize the app.
-     */
-    if (module.hot) {
-        module.hot.accept(
-            [
-                '../../reactium_modules/@atomic-reactor/reactium-core/dependencies/index.js',
-                '../../reactium_modules/@atomic-reactor/reactium-core/app.js',
-                '../../reactium_modules/@atomic-reactor/reactium-core/sdk/index.js',
-            ],
-            () => {
-                window.location.reload();
-            },
-        );
-    }
 })();
