f53837a8feed4eb0bb2a9b888adc0c08265c6b8b
John Dillick <john@dillick.us>
2023-05-19 01:04:39 -0400

    @atomic-reactor/reactium-core v5.0.10

--- src/app/api/domain.js
+++
@@ -1,9 +0,0 @@
-/**
- * -----------------------------------------------------------------------------
- * DDD Domain API - Change name to place domain artifacts in this directory
- * in a different domain.
- * -----------------------------------------------------------------------------
- */
-module.exports = {
-    name: 'API',
-};
